edf9c3e94da55ca0647469b169c37e8a44837fce
alig12622com-crypto <alig12622.com@gmail.com>
2025-07-23 01:16:28 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID SHABAN-MD~cAZwkaJI#19gAoMXUXRk-0bnSHhhCqjUPIL0hUETgkCrb7-61kpA,
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
